827a80c7be2b161f9fae74a5bad46ed77302a929
0LIN1ISA <0LIN1ISA@protonmail.com>
2018-03-08 10:40:18 +0800

    第1章 起步

--- Chapter_1.py
+++ Chapter_1.py
@@ -5,4 +5,16 @@
 
 # 第1章 起步
 
+# 运行Python代码片段
 print("Hello Python interpreter!")
+
+# Hello World程序
+print("Hello world!")
+
+# 检查Python版本
+import sys
+
+print(sys.version)
+# 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)]
+print(sys.version_info)
+# sys.version_info(major=3, minor=6, micro=4, releaselevel='final', serial=0)
